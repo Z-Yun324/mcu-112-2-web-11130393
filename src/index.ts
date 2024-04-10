683108ff10ca6a6558d74d1bf25ed040dca12b68
ZY324 <emma93324>
2024-04-10 17:26:33 +0800

    feat(ts): 利用聯集變數紀錄產品修改日期

--- src/index.ts
+++ src/index.ts
@@ -11,3 +11,6 @@ console.log("Is Show = ", isShow);
 
 let createDate = new Date();
 console.log("Create Date", createDate);
+
+let modifyDate: Date | undefined = undefined;
+console.log("Modify Date", modifyDate);
