ae20e0d5626ddbbf71ab1a87e116ca112647e992
ZY324 <emma93324>
2024-04-10 17:18:59 +0800

    feat(ts): 利用變數紀錄產品資訊

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
-//let 可以改
+let productId: number;
+productId = 1;
+console.log("Product Id = ", productId);
+
+//let 可以改  const 不能改
 let productNg = "圖像 Angular 開發入門 第二版";
-console.log(productNg);
-//const 不能改
-const productCss = "金魚都能懂的 CSS 必學屬性";
-console.log(productCss);
+console.log("Product Name = ", productNg);
+
+const isShow = true;
+console.log("Is Show = ", isShow);
