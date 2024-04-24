edbf84a4c11389174e30c55863f0cfa8177045ee
ZY324 <emma93324>
2024-04-24 18:57:24 +0800

    feat(product): 利用 CurrencyPipe 顯示售價

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,6 +16,7 @@ export class AppComponent {
   isShow = true;
   imgUrl = 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img';
   createDate = new Date();
+  price = '10000';
 
   /*onIsShowChange(isShow: boolean): void {
     this.isShow = isShow;
--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -1,10 +1,10 @@
-import { DatePipe } from '@angular/common';
+import { CurrencyPipe, DatePipe } from '@angular/common';
 import { Component, EventEmitter, Input, Output, booleanAttribute, numberAttribute } from '@angular/core';
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [DatePipe],
+  imports: [DatePipe, CurrencyPipe],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.css',
 })
@@ -16,6 +16,7 @@ export class ProductCardComponent {
   @Input() imgUrl!: string;
   @Input({ transform: booleanAttribute }) isShow!: boolean;
   @Input() createDate!: Date;
+  @Input({ transform: numberAttribute }) price!: number;
 
   @Output() isShowChange = new EventEmitter<boolean>();
 
