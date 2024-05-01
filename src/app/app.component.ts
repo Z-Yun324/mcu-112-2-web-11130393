e46f9b59aef8b5d141dc2f8201320bf6c5fb8e6d
ZY324 <emma93324>
2024-05-01 19:08:35 +0800

    feat(product): 變更作者資訊為文字陣列

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,7 +17,7 @@ export class AppComponent {
     new Product({
       id: 1,
       name: '書籍 A',
-      authors: '作者甲、作者乙、作者丙',
+      authors: ['作者甲', '作者乙', '作者丙'],
       company: '博碩文化',
       isShow: true,
       imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
@@ -27,7 +27,7 @@ export class AppComponent {
     new Product({
       id: 2,
       name: '書籍 B',
-      authors: '作者甲、作者乙、作者丙',
+      authors: ['作者甲', '作者乙', '作者丙'],
       company: '博碩文化',
       isShow: true,
       imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
@@ -37,7 +37,7 @@ export class AppComponent {
     new Product({
       id: 3,
       name: '書籍 C',
-      authors: '作者甲、作者乙、作者丙',
+      authors: ['作者甲', '作者乙', '作者丙'],
       company: '博碩文化',
       isShow: true,
       imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
@@ -47,7 +47,7 @@ export class AppComponent {
     new Product({
       id: 4,
       name: '書籍 D',
-      authors: '作者甲、作者乙、作者丙',
+      authors: ['作者甲', '作者乙', '作者丙'],
       company: '博碩文化',
       isShow: true,
       imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
@@ -65,7 +65,7 @@ export class AppComponent {
       new Product({
         id: 1,
         name: '書籍 A',
-        authors: '作者甲、作者乙、作者丙',
+        authors: ['作者甲', '作者乙', '作者丙'],
         company: '博碩文化',
         isShow: true,
         imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
@@ -75,7 +75,7 @@ export class AppComponent {
       new Product({
         id: 2,
         name: '書籍 B',
-        authors: '作者甲、作者乙、作者丙',
+        authors: ['作者甲', '作者乙', '作者丙'],
         company: '博碩文化',
         isShow: true,
         imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
@@ -85,7 +85,7 @@ export class AppComponent {
       new Product({
         id: 3,
         name: '書籍 C',
-        authors: '作者甲、作者乙、作者丙',
+        authors: ['作者甲', '作者乙', '作者丙'],
         company: '博碩文化',
         isShow: true,
         imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
@@ -95,7 +95,7 @@ export class AppComponent {
       new Product({
         id: 4,
         name: '書籍 D',
-        authors: '作者甲、作者乙、作者丙',
+        authors: ['作者甲', '作者乙', '作者丙'],
         company: '博碩文化',
         isShow: true,
         imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
