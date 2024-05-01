4cea4cc2c3abaceab4cb653e72bee37ff6d99ca6
ZY324 <emma93324>
2024-05-01 18:28:47 +0800

    feat(product): 建立產品資料模型

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -19,6 +19,7 @@ export class ProductCardComponent {
   @Input({ transform: numberAttribute }) price!: number;
 
   @Output() isShowChange = new EventEmitter<boolean>();
+
   @HostBinding('class')
   class = 'product-card';
 
