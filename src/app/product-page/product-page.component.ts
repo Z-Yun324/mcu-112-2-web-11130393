4ab1a2caf97decda9a48ffe4baaaf3b9ba7c7e58
ZY324 <emma93324>
2024-05-15 16:49:34 +0800

    feat(product-service): 變更產品資料新增、修改與刪除回傳型別

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -32,7 +32,7 @@ export class ProductPageComponent implements OnInit {
       createDate: new Date(),
       price: '10000',
     });
-    this.productService.add(product);
+    this.productService.add(product).subscribe();
   }
 
   onView(product: Product): void {
@@ -44,6 +44,6 @@ export class ProductPageComponent implements OnInit {
   }
 
   onRemove({ id }: Product): void {
-    this.productService.remove(id);
+    this.productService.remove(id).subscribe();
   }
 }
